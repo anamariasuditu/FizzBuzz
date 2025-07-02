2c61afe40b76e1d55410c12f245c8294c024e327
AnaSud <Ana-Maria.Suditu@softwire.com>
2025-07-02 10:59:40 +0300

    done FizzBuzz Part2 1)

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 function verify(nr: number): void{
-    if(nr % 3 == 0 && nr % 5 != 0)
-        console.log('Fizz');
-    else if(nr % 5 == 0 && nr % 3 != 0)
-        console.log('Buzz');
-    else if (nr % 3 == 0 && nr % 5 == 0)
+    if(nr % 3 == 0 && nr % 5 != 0){
+        if(nr % 7 == 0)
+            console.log('FizzBang');
+        else
+            console.log('Fizz');
+    }
+    else if(nr % 5 == 0 && nr % 3 != 0){
+        if(nr % 7 == 0)
+            console.log('BuzzBang');
+        else
+            console.log('Buzz');
+    }
+    else if (nr % 15 == 0)
         console.log('FizzBuzz');
+    else if (nr % 7 == 0)
+        console.log('Bang');
     else console.log(nr);
 }
 
